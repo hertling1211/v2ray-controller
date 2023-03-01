d64c282b892649d4689bf1f713b512216e47be98
hertling <1584064142@qq.com>
2023-03-01 14:17:13 +0800

    v2ray-control v0.1

---
+++ src/main/java/com/tianqe/v2raycontroller/V2rayControllerApplication.java
@@ -0,0 +1,13 @@
+package com.tianqe.v2raycontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class V2rayControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(V2rayControllerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tianqe/v2raycontroller/V2rayControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tianqe.v2raycontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class V2rayControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
