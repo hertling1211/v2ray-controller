bedd1d07c615fb70766a686e36eba3daa1bb0a48
hertling <1584064142@qq.com>
2023-03-02 21:44:46 +0800

    更新页面交互内容

--- src/main/java/com/tianqe/v2raycontroller/controller/modifyConfigController/v2rayControl.java
+++ src/main/java/com/tianqe/v2raycontroller/controller/modifyConfigController/v2rayControl.java
@@ -2,6 +2,7 @@ package com.tianqe.v2raycontroller.controller.modifyConfigController;
 
 import com.tianqe.v2raycontroller.service.modifyConfigService.modifyTheConfig;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,11 +17,14 @@ public class v2rayControl {
     /**
      * 上传配置，修改配置内容
      */
+    @Value("${server.port}")
+    private String appPort;
     @GetMapping(value = "/modifyConfig")
     public ModelAndView postConfig(){
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("adminRoot");
-        modelAndView.addObject(modifyConfig.getProxyConfig());
+        modelAndView.addObject("message", modifyConfig.getProxyConfig());
+        modelAndView.addObject("appPort",appPort);
         return modelAndView;
     }
 
