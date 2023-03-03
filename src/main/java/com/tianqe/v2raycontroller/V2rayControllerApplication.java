bda92c84efe3460ec67f5522e4f3dfc73fefbf4d
hertling <1584064142@qq.com>
2023-03-03 12:09:14 +0800

    增加系统判断以及软件安装判断

--- src/main/java/com/tianqe/v2raycontroller/V2rayControllerApplication.java
+++ src/main/java/com/tianqe/v2raycontroller/V2rayControllerApplication.java
@@ -1,12 +1,14 @@
 package com.tianqe.v2raycontroller;
 
+import com.tianqe.v2raycontroller.service.checkInstallService.checkingSoftwareInstallationForLinux;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class V2rayControllerApplication {
-
     public static void main(String[] args) {
+        // 启动检查是否安装有v2ray以及nginx
+        checkingSoftwareInstallationForLinux.checkNginxInstallation();
         SpringApplication.run(V2rayControllerApplication.class, args);
     }
 
--- src/main/java/com/tianqe/v2raycontroller/service/checkInstallService/checkingSoftwareInstallationForLinux.java
+++ src/main/java/com/tianqe/v2raycontroller/service/checkInstallService/checkingSoftwareInstallationForLinux.java
@@ -1,30 +1,63 @@
 package com.tianqe.v2raycontroller.service.checkInstallService;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 /**
- * 检查软件是否已将安装完毕
+ * 判断系统为windows系统还是Linux系统
+ * 判断系统内是否安装有v2ray以及nginx
  * @author hertling
  * @version 1.0
  * @since 2023年3月3日
  */
+@Service
 public class checkingSoftwareInstallationForLinux {
-    public static boolean checkNginxInstallation() {
+
+    public static void checkNginxInstallation() {
+        Process process = null;
+        // 默认为Linux系统
+        boolean judgment = true;
         try {
-            Process process = Runtime.getRuntime().exec("which nginx");
+            // 判断为windows还是Linux
+            String osName = System.getProperty("os.name").toLowerCase();
+            if (osName.contains("win")) {
+                // 当前系统为 Windows，输入cmd命令，在path指定的路径总查找相关可执行文件，如果path中未指定则会提示未找到
+                ProcessBuilder pb = new ProcessBuilder("cmd.exe", "/c", "where nginx");
+                process = pb.start();
+                // 改为false,标识为windows，下载windows版nginx
+                judgment = false;
+            } else {
+                // 当前系统为 Linux 或其他系统
+                process = Runtime.getRuntime().exec("which nginx");
+            }
             BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
             String line;
+            boolean v2rayInstalled = false;
             while ((line = reader.readLine()) != null) {
-                if (!line.isEmpty()) {
-                    return true;
+                if (judgment){
+                    // 文件存在表示已经安装好
+                    if (!line.isEmpty()) {
+                        v2rayInstalled = true;
+                        break;
+                    }
+                }
+                if (line.contains("v2ray.exe")) {
+                    v2rayInstalled = true;
+                    break;
                 }
             }
-            return false;
+            if (v2rayInstalled) {
+                System.out.println("软件已经安装");
+            }else {
+                System.out.println("软件未安装");
+            }
         } catch (IOException e) {
             // handle exception
-            return false;
+            e.printStackTrace();
         }
     }
 
