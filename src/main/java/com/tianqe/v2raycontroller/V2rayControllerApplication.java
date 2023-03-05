2196e9441491959dd5225be5f3953ec86f9196f4
hertling <1584064142@qq.com>
2023-03-05 17:00:34 +0800

    修改readme部分内容

--- src/main/java/com/tianqe/v2raycontroller/V2rayControllerApplication.java
+++ src/main/java/com/tianqe/v2raycontroller/V2rayControllerApplication.java
@@ -7,10 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class V2rayControllerApplication {
     public static void main(String[] args) {
+        SpringApplication.run(V2rayControllerApplication.class, args);
         // 启动检查是否安装有v2ray以及nginx
         checkingSoftwareInstallationForLinux.checkNginxInstallation();
 
-        SpringApplication.run(V2rayControllerApplication.class, args);
     }
 
 }
